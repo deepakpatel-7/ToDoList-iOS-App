0c2e30d3c5e042e7d67146eb49a8e89dc9dbc969
deepakpatel-7 <deepakpatel6957@gmail.com>
2024-05-27 17:11:50 +0530

    initial commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Deepak Patel on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
